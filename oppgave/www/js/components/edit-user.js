46be4ecb9b2a039ecc07444cff442a0544b26088
jonakj <jonakj@stud.ntnu.no>
2020-12-17 11:22:24 +0100

    Added basic form, needs rework

--- oppgave/www/js/components/edit-user.js
+++ oppgave/www/js/components/edit-user.js
@@ -8,6 +8,20 @@ class EditUser extends LitElement {
   }
 
   // din kode her
+  
+  
+  render() {
+	return html`
+	<form id="form1">
+	Username: <input type="text" name="uname"><br>
+	First name: <input type="text" name="firstName"><br>
+	Last name: <input type="text" name="lastName"><br>
+	Password: <input type="text" name="password"><br>
+	</form>
+	<br>
+	<button type="submit" form="form1" value="Submit">Submit</button>
+	`;
+  }
 
 }
 customElements.define('edit-user', EditUser);
