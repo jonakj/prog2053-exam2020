598558786289b048d91e3ef08808900106c3d68f
jonakj <jonakj@stud.ntnu.no>
2020-12-17 12:49:48 +0100

    Changing first and last name now works

--- oppgave/www/js/components/edit-user.js
+++ oppgave/www/js/components/edit-user.js
@@ -7,18 +7,44 @@ class EditUser extends LitElement {
     };
   }
 
-// Renders / adds the form to the html page
+// Renders / adds the form to the html page, the submit button runs the "changeUserData" command
   render() {
 	return html`
-	<form id="form1" method="POST">
-	Username: <input type="text" name="uname" value="${this.user.uname}"><br>
-	First name: <input type="text" name="firstName" value="${this.user.firstName}"><br>
-	Last name: <input type="text" name="lastName" value="${this.user.lastName}"><br>
-	Password new: <input type="text" name="password"><br>
-	Password old: <input type="text" name="password"><br>
-	<input type="submit" form="form1" value="Submit">
+	<form onsubmit="javascript: return false;" id="form1" method="POST">
+	UID: <input type="text" id="uid" name="uid" value="${this.user.uid}" readonly>
+	<br>
+	Username: <input type="text" id="uname" name="uname" value="${this.user.uname}">
+	<br>
+	First name: <input type="text" id="firstName" name="firstName" value="${this.user.firstName}">
+	<br>
+	Last name: <input type="text" id="lastName" name="lastName" value="${this.user.lastName}">
+	<br>
+	Password new: <input type="text" id="pwd" name="pwd">
+	<br>
+	Password old: <input type="text" id="oldpwd" name="oldpwd">
+	<br>
+	<input type="submit" form="form1" value="Submit" @click=${this.changeUserData}>
 	`;
   }
-	
+
+  //Runs on submit button click
+  changeUserData(i) {
+    // Retrieves the data from the form
+    const userData = new FormData(i.target.form);
+	// Uses the updateUser php script with the form data
+    fetch('api/updateUser.php', {
+     method: 'POST',
+     body: userData
+    }).then(res=>res.json())
+      .then(data=>{
+        if (data.status=='success') {
+            console.log("User info modified");
+        } else {
+            console.log("The user info not modified due to an error");
+			// Error message from the php script
+			console.log("Error: " + data.msg);
+        }
+      })
+  }
 }
 customElements.define('edit-user', EditUser);
--- readme.md
+++ readme.md
@@ -1,4 +1,4 @@
-eksamensnummer 10087
+Candidate number 10087
 
 # PROG2053 eksamen v2020
 * Lag en FORK av dette repositoriet og gi meg leserettigheter (aland.a.mendoza@ntnu.no). Det som er i repositoriet på slutten av eksamen teller som din besvarelse.
@@ -13,3 +13,5 @@ Lag en side som lister ut alle brukere. Hent brukere fra api/fetchUsers.php med 
 I denne oppgaven skal du lage samme funksjonaliteten med web komponenter. I filen oppgave3.html finner du startpunktet for denne oppgaven. Denne filen bruker users-list.js komponenten i js/components mappen til å lage en liste av alle brukere, funksjonaliteten i denne komponenten er ferdig laget. I render (på linje 56) brukes en edit-user.js komponent, denne komponenten får tilsendt den valgte brukeren. Det som mangler i edit-user.js er funksjonaliteten for å oppdatere en bruker.
 
 NB, i oppgave/www/node_modules ligger lit-element med de endringer som skal til for at det fungerer uten å bruke "polymer serve" (dvs, koden fungerer direkte i docker/XAMPP).
+
+Could not get the updateUser.php script to change the password or username in testing, might be an environment error on my part. I did however get it to update both the first and lastnames of the user.
